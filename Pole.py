1f6103830cdd32eb98e85a687d283740441ba5c2
JohnOlsea <91258573+JohnOlsea@users.noreply.github.com>
2023-02-16 18:29:46 +0700

    Pole

---
+++ Pole.py
@@ -0,0 +1,18 @@
+class Pole(object):
+    def __init__(self,name="",xpos = 0,ypos = 0,thick = 10, length = 100):
+        self.pname = name
+        self.stack = []
+        self.toppos = 0
+        self.pxpos = xpos
+        self.pypos = ypos
+        self.pthick = thick
+        self.plength = length
+
+    def showpole(self):
+        pass
+
+    def pushdisk(self):
+        pass
+
+    def popdisk(self):
+        pass
