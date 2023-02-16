14e1b5201a3dea5812fad88c209b21027dd840f7
Kumpob <90686608+Kumpob@users.noreply.github.com>
2023-02-16 18:48:32 +0700

    Update Disk.py

--- Disk.py
+++ Disk.py
@@ -1,39 +1,40 @@
 import turtle
+
 class Disk(object):
-    def __init__(self, name="",xpos=0,ypos=0,height=20,width=40):
-        self.dname=name
-        self.dxpos=xpos
-        self.dypos=ypos
-        self.dheight=height
-        self.dwidth=width
+    def __init__(self, name="", xpos = 0, ypos = 0, height = 20, width = 40):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+        self.rect = turtle.Turtle()
 
     def showdisk(self):
-        turtle.setheading(90)
-        turtle.forward(self.dwidth/2)
-        turtle.left(90)
-        turtle.forward(self.dheight)
-        turtle.left(90)
-        turtle.forward(self.dwidth)
-        turtle.left(90)
-        turtle.forward(self.dheight)
-        turtle.left(90)
-        turtle.forward(self.dwidth/2)
+        screen = self.rect.getscreen()
+        self.rect.setheading(0)
+        self.rect.penup()
+        self.rect.goto(self.dxpos, self.dypos)
+        self.rect.pendown()
 
-    def newpos(self,xpos,ypos):
-        turtle.penup()
-        turtle.setpos(xpos,ypos)
-        turtle.pendown()
-        
+        self.rect.begin_fill()
+        for i in range(2):
+            self.rect.forward(self.dwidth / 2)
+            self.rect.lt(90)
+            self.rect.forward(self.dheight)
+            self.rect.lt(90)
+            self.rect.forward(self.dwidth / 2)
+        self.rect.end_fill()
+
+
+
+    def newpos(self, xpos, ypos):
+        self.dxpos = xpos
+        self.dypos = ypos
+
+
     def cleardisk(self):
-        turtle.pencolor("white")
-        turtle.setheading(90)
-        turtle.forward(self.dwidth/2)
-        turtle.left(90)
-        turtle.forward(self.dheight)
-        turtle.left(90)
-        turtle.forward(self.dwidth)
-        turtle.left(90)
-        turtle.forward(self.dheight)
-        turtle.left(90)
-        turtle.forward(self.dwidth/2)
-        turtle.pencolor("black")
+        self.rect.setheading(0)
+        self.rect.clear()
+        # self.rect.done()
+
+
