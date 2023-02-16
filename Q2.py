6b25c02db44ab169207e74fdcec2103fb6e9b8d4
JohnOlsea <91258573+JohnOlsea@users.noreply.github.com>
2023-02-16 17:59:48 +0700

    update mouse event function

--- Q2.py
+++ Q2.py
@@ -16,3 +16,21 @@ class DrawingWindow(QWidget):
         self.image = QImage(self.size(), QImage.Format_RGB32)
         self.image.fill(Qt.white)
         
+    def mousePressEvent(self, event):
+        if (event.button() == Qt.LeftButton):
+            self.drawing = True
+            self.previousPoint = event.pos()
+
+    def mouseMoveEvent(self, e):
+        if (e.buttons() and Qt.LeftButton) and self.drawing:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(Qt.black, self.brushSize))
+
+            painter.drawLine(self.previousPoint, e.pos())
+
+            self.previousPoint = e.pos()
+            self.update()
+
+    def mouseReleaseEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            self.drawing = False
