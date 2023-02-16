bf5bd1f6823d8a9b4a0b4e43b8b4feec80f9a14a
JohnOlsea <91258573+JohnOlsea@users.noreply.github.com>
2023-02-16 18:39:52 +0700

    Pole update

--- Pole.py
+++ Pole.py
@@ -1,6 +1,8 @@
-from Disk import *
+import turtle
+from disk import Disk
+
 class Pole(object):
-    def __init__(self,name="",xpos = 0,ypos = 0,thick = 10, length = 100):
+    def __init__(self, name="", xpos=0, ypos=0, thick=10, length=100):
         self.pname = name
         self.stack = []
         self.toppos = 0
@@ -10,10 +12,33 @@ class Pole(object):
         self.plength = length
 
     def showpole(self):
-        pass
+        turtle.penup()
+        turtle.goto(self.pxpos, self.pypos)
+        turtle.pendown()
 
-    def pushdisk(self):
-        pass
+        turtle.forward(self.pthick/2)
+        turtle.left(90)
+        turtle.forward(self.plength)
+        turtle.left(90)
+        turtle.forward(self.pthick)
+        turtle.left(90)
+        turtle.forward(self.plength)
+        turtle.left(90)
+        turtle.forward(self.pthick/2)
+
+    def pushdisk(self, disk):
+        disk.newpos(self.pxpos, self.toppos)
+        disk.showdisk()
+
+        self.toppos += disk.dheight
+        self.stack.append(disk)
 
     def popdisk(self):
-        pass
+        outputdisk = self.stack.pop()
+        self.toppos -= outputdisk.dheight
+
+        outputdisk.newpos(self.pxpos, self.toppos)
+        outputdisk.cleardisk()
+
+myPole = Pole()
+myPole.showpole()
